4725dad03afcd7c2679a3ba6667289797420e292
Mellycal <105233176+Mellycal@users.noreply.github.com>
2022-05-27 22:57:50 +0800

    changes

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+
+    def __init__(self, name, age, tracks, score):
+
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def details(self):
+        print('Name', self.name)
+        print('Age', self.age)
+        print('Tracks', self.tracks)
+        print('Score', self.score)
+
+    bob = ("Bob", "26", "FE", "BE", "20.90")
+
+
+
+# Expected methods
+    print('Bob.change_name' "Peter")
+    print('Bob.change_age' "34")
+    print('Bob.add_track' "UI/UX")
+    print('Bob.get_score' "20.90")
